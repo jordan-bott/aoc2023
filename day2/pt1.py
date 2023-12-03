acb8591e217bf17b25f1689b8da0d0c4e5996569
Jordan Bott <jordan.bott77@gmail.com>
2023-12-02 16:17:14 -0800

    remove debugging print statements

--- day2/pt1.py
+++ day2/pt1.py
@@ -12,30 +12,21 @@ for game in input:
     blue = True
     for turn in game_list:
         turn_list = turn.split()
-        # print(turn_list)
         for c in range(1, len(turn_list), 2):
             item = turn_list[c]
-            # print(item, turn_list[c-1])
             if item in red_list and int(turn_list[c-1]) > 12:
-                # print("\nred fail")
                 red = False
                 break
             elif item in green_list and int(turn_list[c-1]) > 13:
-                # print("\ngreen fail")
                 green = False
                 break
             elif item in blue_list and int(turn_list[c-1]) > 14:
-                # print("\nblue fail")
                 blue = False
                 break
         if red == False or blue == False or green == False:
             break
-    # print("\n\n---------------", game)
     if red == True and blue == True and green == True:
-        # print("++++++++++++++++++++++++")
-        print(game, "            ", game_list)
         result += int(game)
-    # print(result)
 
 
 print(result)
