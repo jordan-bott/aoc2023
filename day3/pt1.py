539e535245bc6a0fec678abbd1239d0fe6ec5fc0
Jordan Bott <jordan.bott77@gmail.com>
2023-12-04 11:18:53 -0800

    day 3 in progress

--- day3/pt1.py
+++ day3/pt1.py
@@ -1,15 +1,4 @@
-# from input import input
-
-input = '''467..114..
-...*......
-..35..633.
-......#...
-617*......
-.....+.58.
-..592.....
-......755.
-...$.*....
-.664.598..'''
+from input import input
 
 
 num_list = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
@@ -21,27 +10,27 @@ input_list = input.splitlines()
 
 # builds symbol location dictionary
 # keys are the number list, and the value is a list of number locations
-for x in range(len(input_list)-1):
+for x in range(len(input_list)):
     line = input_list[x]
     symbol_dict[x] = []
     for i in range(len(line)):
         if line[i] in symbol_list:
             symbol_dict[x].append(i)
 
-for y in range(len(input_list)-1):
+for y in range(len(input_list)):
     line = list(input_list[y])
+    print("\nline", line)
     for z in range(len(line)-1):
         part_number_check = False
         part_number = None
         if line[z] in num_list:
-            print("line z", line[z])
+            print("number", line[z])
             if z > 0:
                 # left: line[z-1]
                 if line[z-1] in symbol_list:
-                    print("/nyes")
                     part_number_check = True
                     part_number = line[z]
-            if z < len(line[z]) - 1:
+            if z < len(line) - 1:
                 # right: line[z+1]
                 if line[z+1] in symbol_list:
                     part_number_check = True
@@ -57,14 +46,13 @@ for y in range(len(input_list)-1):
                         part_number_check = True
                         part_number = line[z]
 
-                if z < len(line[z]):
+                if z < len(line):
                     # above-right: input_list[y-1][z+1]
                     if input_list[y-1][z+1] in symbol_list:
                         part_number_check = True
                         part_number = line[z]
 
             if y < len(input_list) - 1:
-
                 # bottom: input[y+1]
                 if input_list[y+1] in symbol_list:
                     part_number_check = True
@@ -74,29 +62,30 @@ for y in range(len(input_list)-1):
                     if input_list[y+1][z-1] in symbol_list:
                         part_number_check = True
                         part_number = line[z]
-
-                if z < len(line[z]):
+                if z < len(line):
                     # bottom-right: input[y+1][z+1]
                     if input_list[y+1][z+1] in symbol_list:
                         part_number_check = True
                         part_number = line[z]
 
         if part_number_check == True:
-            print(part_number, "\n first partnum")
+            print("number check", part_number)
             for num in range(z + 1, len(line)-1):
+                print("______", line[num])
                 if line[num] in num_list:
                     part_number += line[num]
                     line[num] = "."
                 else:
                     break
-            for num in range(len(line)-1, z - 1, -1):
+            for num in range(z-1, -1, -1):
+                print("+++++++++", line[num])
                 if line[num] in num_list:
                     part_number = line[num] + part_number
                     line[num] = "."
                 else:
                     break
         if part_number != None:
-            print(part_number)
+            print("adding!!!!+++++++", part_number)
             result += int(part_number)
 
 print("\n\n\n result:", result)
